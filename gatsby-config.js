db666b5ecc84d93ea4ad6eb6c04cbfb33b22f3ea
ArthurEstevan <arthur.estevan01@gmail.com>
2022-07-02 00:23:32 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,13 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `My blog`,
+    description: `A blog about frontend developmente and other cool stuff`,
+    author: `@myblog`,
+    siteUrl: `http://localhost:8000/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
